9cef078f1172d28a896158af028574647ca26007
fuyunwang <2375872953@qq.com>
2022-05-04 12:13:18 +0800

    add hot100

---
+++ src/main/java/com/fyw/algorithms/hot100/Code617.java
@@ -0,0 +1,10 @@
+package com.fyw.algorithms.hot100;
+
+/**
+ * @Title Code617
+ * @Author fyw
+ * @Date 2022/5/4 12:11
+ * @Description:
+ */
+public class Code617 {
+}
---
+++ src/main/java/com/fyw/algorithms/hot100/Code647.java
@@ -0,0 +1,10 @@
+package com.fyw.algorithms.hot100;
+
+/**
+ * @Title Code647
+ * @Author fyw
+ * @Date 2022/5/4 12:11
+ * @Description:
+ */
+public class Code647 {
+}
